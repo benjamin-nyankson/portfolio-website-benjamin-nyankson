d2dd70a747d0fd37af3fd04863cdea7761bb967e
Benjamin Nyankson <117347759+nyankson-benjamin@users.noreply.github.com>
2024-07-31 23:14:21 +0000

    feat: add e-shop

--- src/components/views/ProjectView.vue
+++ src/components/views/ProjectView.vue
@@ -18,15 +18,17 @@ import youtube from "@/assets/youtube-clone.png"
 import eCommerce from "@/assets/e-commerce.png"
 import reactImg from "@/assets/react-png.png"
 import timeline from "@/assets/timeline.png"
+import ecom from "@/assets/e-com.png"
 import SkillsCard from '../SkillsCard.vue';
 import  useSetInterval  from '@/composables/useSetInterval';
 
 const listOfProjects = ref([
-    { name: "E-learning", id: 1, type: "Vue", img_url:learn, link: "https://e-learning-dashboard-31k8xgwp6-nyankson-benjamin.vercel.app" },
-    { name: "Mini E-commerce", id: 2, type: "vue", img_url:eCommerce, link: "https://e-commerce-app-vue.vercel.app/" },
-    { name: "Youtube Clone", id: 3, type: "vue", img_url:youtube, link: "https://youtube-three-delta.vercel.app", },
-    { name: "Project Time Line", id: 4, type: "vue", img_url:timeline, link: "https://time-line-livid.vercel.app/" },
-    { name: "Travelling App", id: 5, type: "vue", img_url:reactImg, link: "https://travelling-app-rho.vercel.app/" },
+    { name: "Project Time Line", id: 1, type: "vue", img_url:timeline, link: "https://time-line-livid.vercel.app/" },
+    { name: "E-shop", id: 2, type: "vue", img_url:ecom, link: "https://e-com-three-tan.vercel.app/" },
+    { name: "E-learning", id: 3, type: "Vue", img_url:learn, link: "https://e-learning-dashboard-31k8xgwp6-nyankson-benjamin.vercel.app" },
+    { name: "Mini E-commerce", id: 4, type: "vue", img_url:eCommerce, link: "https://e-commerce-app-vue.vercel.app/" },
+    { name: "Youtube Clone", id: 5, type: "vue", img_url:youtube, link: "https://youtube-three-delta.vercel.app", },
+    { name: "Travelling App", id: 6, type: "vue", img_url:reactImg, link: "https://travelling-app-rho.vercel.app/" },
 
 ])
 
